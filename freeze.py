7bbffea046e496442dfbaa253db5c10538735103
Tom Hall <thall-tom@hotmail.co.uk>
2021-05-16 21:28:43 +0200

    Trial ignoring img folder and uploading to seperate netlify site

--- freeze.py
+++ freeze.py
@@ -5,6 +5,7 @@ from flask_frozen import Freezer
 from flask import url_for
 import http.server as svr
 import os
+import subprocess
 
 freezer = Freezer(app)
 
@@ -18,6 +19,10 @@ freezer = Freezer(app)
 if __name__ == '__main__':
     # Freeze static files into default directory 'build'
     freezer.freeze()
+    print("Website frozen")
+    # cmnd = "python -m pynetlify deploy_folder --site-id bd867c99-8ad2-41da-b295-d619581e8079 website/static/img/"
+    # res = subprocess.call(cmnd, shell = True)
+    # print("Pynetlify deploy folder exit code: ", res)
     # Use python builtin server to serve static files based on directory structure
     os.chdir('website/build')
     svr.test(HandlerClass=svr.SimpleHTTPRequestHandler, port=5001)
