f39934ded35c302a57764fede6ea0ec9d0aa0074
Tom Hall <thall-tom@hotmail.co.uk>
2021-09-25 18:23:20 -0500

    refactor most classes to o- c- prefixes

--- website/static/js/site_nav.js
+++ website/static/js/site_nav.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", siteNav);
 function siteNav() {
 
   // Select the menu icon
-  const menuIcon = document.querySelector(".site_nav--btn");
-  const menu = document.querySelector(".site_nav--menu");
+  const menuIcon = document.querySelector(".js-navBtn");
+  const menu = document.querySelector(".js-navMenu");
 
-  menuIcon.classList.remove('hidden');
-  menu.classList.remove('open');
+  menuIcon.classList.remove('is-hidden');
+  menu.classList.remove('is-open');
 
   // For each element, add a listener for the "click" event.
   menuIcon.addEventListener("click", toggleMenu);
 
   function toggleMenu(e) {
-    menu.classList.toggle('open');
+    menu.classList.toggle('is-open');
   };
 
 }
