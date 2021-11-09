b889ffab5c85de0d539b8a756378fa81262230da
Tom Hall <thall-tom@hotmail.co.uk>
2021-11-09 23:57:23 +0000

    create js folder if not already present

--- scripts/install.py
+++ scripts/install.py
@@ -1,5 +1,6 @@
 # Download build and store latest alpine.js files in static,
 # choosing type by config
+import os
 import requests
 
 from website import app
@@ -8,8 +9,8 @@ alpine_url = app.config["ALPINE"]['url']
 alpine_file_path = app.config["ALPINE"]['fpath']
 
 js = requests.get(alpine_url)
-# js = f"console.log({alpine_url})"
 
 if js.ok:
+    os.makedirs(os.path.dirname(alpine_file_path), exist_ok=True)
     with open(alpine_file_path, 'w') as f:
         f.write(js.text)
