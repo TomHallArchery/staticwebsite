a6a81b0b16ae6b8b90279d741432d2101b2fd906
Tom Hall <thall-tom@hotmail.co.uk>
2021-11-10 23:36:22 +0000

    Add custom 404 error page to locally served site

--- scripts/serve.py
+++ scripts/serve.py
@@ -34,45 +34,69 @@ CDN_HEADERS = {
     }
 
 
-class ROOT_HTTPReqHandler(svr.SimpleHTTPRequestHandler):
+class MyReqHandler(svr.SimpleHTTPRequestHandler):
+    ''' base custom request handler class '''
+
+
+class ROOTReqHandler(MyReqHandler):
+    ''' custom request handler to send root domain headers'''
+
     def end_headers(self):
         self.send_my_headers()
         super().end_headers()
 
     def send_my_headers(self):
+        ''' override method to send custom headers '''
         for header, value in ROOT_HEADERS.items():
             self.send_header(header, value)
 
+    def _error_page(self):
+        with open('404.html', encoding="utf8") as f:
+            return f.read()
 
-class CDN_HTTPReqHandler(svr.SimpleHTTPRequestHandler):
+    def send_error(self, code, message=None, explain=None):
+        if code == 404:
+            print("Custom 404 handler")
+            self.error_message_format = self._error_page()
+        super().send_error(code, message, explain)
+
+
+class CDNReqHandler(MyReqHandler):
+    ''' custom request handler to send cdn domain headers'''
+
     def end_headers(self):
         self.send_my_headers()
         super().end_headers()
 
     def send_my_headers(self):
+        ''' override method to send custom headers '''
         for header, value in CDN_HEADERS.items():
             self.send_header(header, value)
 
 
-def serve_static(handler, dir, port):
-    print("Serving: ", dir)
-    with utils.cwd(dir):
+def serve_static(handler, directory, port):
+    ''' serve static files from directory '''
+    print("Serving: ", directory)
+    with utils.cwd(directory):
         svr.test(HandlerClass=handler, port=port)
 
 
 def serve_site():
-    serve_static(ROOT_HTTPReqHandler, 'website/build', 5002)
+    ''' serve frozen site on port 5002 '''
+    serve_static(ROOTReqHandler, 'website/build', 5002)
 
 
 def serve_cdn():
-    serve_static(CDN_HTTPReqHandler, 'website/static/img/out', 5003)
+    ''' serve cdn on port 5003 '''
+    serve_static(CDNReqHandler, 'website/static/img/out', 5003)
 
 
 def main():
-    p1 = multiprocessing.Process(target=serve_site)
-    p2 = multiprocessing.Process(target=serve_cdn)
-    p1.start()
-    p2.start()
+    ''' serve from build directory and img '''
+    proc1 = multiprocessing.Process(target=serve_site)
+    proc2 = multiprocessing.Process(target=serve_cdn)
+    proc1.start()
+    proc2.start()
 
 
 if __name__ == '__main__':
--- website/errors.py
+++ website/errors.py
@@ -2,13 +2,8 @@ from flask import current_app as app
 from flask import render_template
 
 
-def render_404():
-    ''' return 404 page html template '''
-    return render_template('generic/404.html.j2')
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     ''' return 404 status code and template '''
     print(e)
-    return render_404(), 404
+    return render_template('generic/404.html.j2'), 404
