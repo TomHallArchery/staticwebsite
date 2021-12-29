f7a1afd96da49ab8e1f14ddba5af392b1063adf0
Tom Hall <thall-tom@hotmail.co.uk>
2021-12-29 23:24:08 +0000

    add freeze cli command

--- website/cli/__init__.py
+++ website/cli/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 
 cli_bp = Blueprint('cli', __name__, cli_group=None)
 
-from . import develop
+from . import develop, freeze  # noqa
 
 
 @cli_bp.cli.command('config')
--- website/cli/freeze.py
+++ website/cli/freeze.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python
-import os
+from pathlib import Path
 
+import click
 import flask
 from flask_htmlmin import HTMLMIN
 from flask_frozen import Freezer
 
-from website import create_app, utils
 import config
-
-# configure app for freezing locally or for deployment
-conf = os.environ.get("APP_CONFIG")
-if conf == "Deploy":
-    app = create_app(config.DeployConfig)
-else:
-    app = create_app(config.BuildConfig)
-
-HTMLMIN(app)
-freezer = Freezer(app)
+from website import create_app
+from . import cli_bp as bp
+from .utils import compile_css
 
 
-# Instructs the freezer to also check for dynamically generated urls
-# from serve_page functinon.
-@freezer.register_generator
-def register_fonts():
-    ''' Register font files with frozen flask '''
-    fonts = app.config["FONTS"]
-    for path, font in fonts.items():
-        file = os.path.join('fonts', path, font)
-        yield flask.url_for('static', filename=file)
-
-
+@bp.cli.command('freeze')
 def main():
     ''' Freeze website into static files '''
-    print("Building website:")
+
+    app = create_app(config.BuildConfig)
+    HTMLMIN(app)
+    freezer = Freezer(app)
+
+    # Instructs the freezer to also check for dynamically generated urls
+    # from serve_page functinon.
+    @freezer.register_generator
+    def register_fonts():
+        ''' Register font files with frozen flask '''
+        fonts = app.config["FONTS"]
+        for path, font in fonts.items():
+            file = Path('fonts', path, font)
+            yield flask.url_for('static', filename=str(file))
+
+    click.echo("Building website:")
 
     # TODO: check font files exist and compile with pyftsubset
 
     # Freeze static files into default directory 'build'
     freezer.freeze()
-    print("Website frozen")
+    click.echo("Website frozen")
 
-    utils.compile_css(app, compressed=True)
-    print("Css recompiled")
+    compile_css(app, compressed=True)
+    click.echo("Css recompiled")
 
     # Frozen flask issue:
     # have to manually build the 404 error page for use by server
@@ -49,7 +47,3 @@ def main():
         error_page = flask.render_template('generic/404.html.j2')
         with open('website/build/404.html', 'w', encoding="utf-8") as f:
             f.write(error_page)
-
-
-if __name__ == '__main__':
-    main()
